322241f7db955d61354cb9c8bc31e5cabf01b575
kousikd-d <kousik.d@deeptaai.com>
2026-09-02 22:50:55 +0530

    bump: inital version to v1.0.0

--- Package.swift
+++ Package.swift
@@ -5,17 +5,22 @@ import PackageDescription
 
 let package = Package(
     name: "Klaritics",
+    platforms: [
+        .iOS(.v13)
+    ],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
-            name: "klaritics-ios-sdk",
-            targets: ["klaritics-ios-sdk"]),
+            name: "Klaritics",
+            targets: ["Klaritics"]),
     ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
-        .target(
-            name: "klaritics-ios-sdk"),
+        .binaryTarget(name: "Klaritics",
+                      url:  "https://storage.googleapis.com/klaritics-ios-sdk-artifacts/klaritics/1.0.0/Klaritics-1.0.0.zip",
+                      checksum: "ffadddefc5c221d557678f4a5672e62e03a8abd56a24f7a77a521cde80c249e7"
+                     )
 
     ]
 )
